ce3c274a7a8a8b6ee9e41c1306608577cda152ee
Kingsleyrhema <Kingsleyrhema135@gmail.com>
2022-09-09 12:28:44 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
-int main(void)
 /**
- * *main-main block
- * 
- * ("with proper grammar, but the outcome is a piece of art,")
+ * main-main block
+ *
+ * with proper grammar, but the outcome is a piece of art,
  *
  * return: 0
  */
+int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
