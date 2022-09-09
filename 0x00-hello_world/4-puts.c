d8d137c755b84daa62b897d269513a3ddd0629ba
Kingsleyrhema <Kingsleyrhema135@gmail.com>
2022-09-09 13:18:48 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - main block
  *
  * Return: text in printf
- */
+ *i/
 int main(void)
-{puts("Programming is like building a multilingual puzzle\n");
+{puts(""Programming is like building a multilingual puzzle\n");
 	return (0);
 }
