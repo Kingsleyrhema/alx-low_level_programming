44b7f7af883a429e3dc6fc6ad7be3f3a1e57dda3
Kingsleyrhema <Kingsleyrhema135@gmail.com>
2022-09-11 08:37:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * mian - main block
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+
+{
+	int i;
+	int I;
+	for (i = 97; i < 122; i++)
+
+		putchar(i);
+	for (I = 65; i < 91; i++)
+
+		putchar(I);
+	putchar('\n');
+	return (0);
+}
