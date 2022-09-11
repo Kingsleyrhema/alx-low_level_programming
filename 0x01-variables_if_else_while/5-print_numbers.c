e77dcb7d7cf9a613086a419cd3302bf6cade0575
Kingsleyrhema <Kingsleyrhema135@gmail.com>
2022-09-11 10:47:46 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print for 0 - 9
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d \n", n);
+	}
+	return (0);
+}
