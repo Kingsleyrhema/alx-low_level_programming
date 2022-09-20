669b77b013c6d75e1d57d1fc1568bf95ccf820bb
Kingsleyrhema <Kingsleyrhema135@gmail.com>
2022-09-20 03:17:51 -0700

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * swap_int - swap the values pointed to by arguments
- * @a: pointer argument 1 
- * @b: pointer argument 2
+ *@a: pointer argument 1
+ *@b: pointer argument 2
  */
+
 void swap_int(int *a, int *b)
 
 {
